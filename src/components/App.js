a26685a3eab4ed0f01a4fbbf3cbda2c5ad5882d3
Steve Godin <steve.god@gmail.com>
2017-08-11 12:02:00 -0700

    Add QuestionNewPage and friends

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router, // When importing, use `as` to alias imported to something else
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom';
 import QuestionsIndexPage from './pages/QuestionsIndexPage';
 import QuestionsShowPage from './pages/QuestionsShowPage';
+import QuestionsNewPage from './pages/QuestionsNewPage';
 
 class App extends Component {
   render() {
@@ -18,9 +20,12 @@ class App extends Component {
             <Link to='/questions/new'>New Question</Link>
           </nav>
           <h1>Awesome Answers</h1>
-          <Route exact path='/' component={QuestionsIndexPage} />
-          <Route exact path='/questions' component={QuestionsIndexPage} />
-          <Route path='/questions/:id' component={QuestionsShowPage} />
+          <Switch>
+            <Route exact path='/' component={QuestionsIndexPage} />
+            <Route exact path='/questions' component={QuestionsIndexPage} />
+            <Route exact path='/questions/new' component={QuestionsNewPage} />
+            <Route path='/questions/:id' component={QuestionsShowPage} />
+          </Switch>
         </div>
       </Router>
     );
