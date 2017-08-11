1786ebc143fc4351de93c3c70afb6e1ef3d2665e
Steve Godin <steve.god@gmail.com>
2017-08-11 10:43:21 -0700

    Install and setup react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router, // When importing, use `as` to alias imported to something else
+  Route,
+  Link
+} from 'react-router-dom';
 import QuestionsIndexPage from './pages/QuestionsIndexPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Awesome Answers</h1>
-        <QuestionsIndexPage />
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/questions'>Questions</Link>
+            <Link to='/questions/new'>New Question</Link>
+          </nav>
+          <h1>Awesome Answers</h1>
+          <Route exact path='/' component={QuestionsIndexPage} />
+          <Route exact path='/questions' component={QuestionsIndexPage} />
+        </div>
+      </Router>
     );
   }
 }
