75460cd2fd3f72d987ba18d54ae78bd6fce5fa2f
Steve Godin <steve.god@gmail.com>
2017-08-11 10:25:40 -0700

    Add QuestionsIndexPage component and friends

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import QuestionsIndexPage from './pages/QuestionsIndexPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>Awesome Answers</h1>
+        <QuestionsIndexPage />
       </div>
     );
   }
